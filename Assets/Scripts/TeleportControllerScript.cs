ba725cc35389e5ce64f22507ec77b3151e9d3a1a
IndiSnacks <sahilsankur@gmail.com>
2024-04-23 23:34:50 -0700

    feature: teleportation is functional but a little jank

--- Assets/Scripts/TeleportControllerScript.cs
+++ Assets/Scripts/TeleportControllerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,21 +9,34 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TeleportControllerScript : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] 
+    private LineRenderer lineRenderer;
+    [SerializeField] 
+    private XRRayInteractor rayInteractor;
+    [SerializeField] 
+    private GameObject player;
   
 
     public InputActionReference trigger;
     private XRInteractorLineVisual lineVisual;
     private float triggerVal;
+    private Vector3 hitLocation;
+    private Vector3 hitNormal;
+    private Vector3 playerPosition;
 
     private void Awake() {
+        playerPosition = player.transform.position;
+        rayInteractor = GetComponent<XRRayInteractor>();
         lineVisual = GetComponent<XRInteractorLineVisual>();
         lineVisual.enabled = false;
+
         trigger.action.Enable();
+        trigger.action.canceled += Teleport;
     }
 
     private void OnDestroy() {
         trigger.action.Disable();
+        trigger.action.canceled -= Teleport;
     }
     
     private void Update() {
@@ -34,5 +48,15 @@ public class TeleportControllerScript : MonoBehaviour
             lineVisual.enabled = false;
             lineRenderer.enabled = false;
         }
+    }
+
+    /**
+    * On trigger release, teleport the player to the location of the hit object
+    */
+    private void Teleport(InputAction.CallbackContext context) {
+        rayInteractor.TryGetHitInfo(out Vector3 hitlocation, out Vector3 hitnormal, out int positionInLine, out bool isValidTarget);
+        Debug.Log(hitlocation);
+        Vector3 newPosition = new Vector3(hitlocation.x, player.transform.position.y, hitlocation.z);
+        player.transform.position = newPosition;
     }
 }
