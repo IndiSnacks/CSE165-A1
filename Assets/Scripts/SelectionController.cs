d68498132fedfbdc92bdc5287e7c5d96854949fb
IndiSnacks <sahilsankur@gmail.com>
2024-04-24 18:19:11 -0700

    feature: create raycaseter

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -2,63 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class SelectionController : MonoBehaviour
 {
-    [SerializeField]
-    public InputActionReference trigger;
+    [SerializeField] 
+    private LineRenderer lineRenderer;
 
     [SerializeField]
-    private XRInteractorLineVisual line;
+    private GameObject controller;
 
-    private float triggerValue;
+    [SerializeField]
+    private InputActionReference trigger;
 
-    private void Awake() {
-        trigger.action.Enable();
-    }
+    private float triggerVal;
+    private Vector3 hitPosition;
+    private LayerMask layerMask;
 
-    private void OnDestroy() {
-        trigger.action.Disable();
-    }
-
-    private void ColorChnage(){
-        // line.setLineColorGradient(Color.green);
-    }
-
-    private void Update() {
-        triggerValue = trigger.action.ReadValue<float>();
-        if (triggerValue > 0.1f) {
-            Gradient greenGradient = new Gradient();
-            GradientColorKey[] colorKeys = new GradientColorKey[2];
-            colorKeys[0].color = Color.green;
-            colorKeys[0].time = 0f;
-            colorKeys[1].color = Color.green;
-            colorKeys[1].time = 1f;
-            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
-            alphaKeys[0].alpha = 1f;
-            alphaKeys[0].time = 0f;
-            alphaKeys[1].alpha = 1f;
-            alphaKeys[1].time = 1f;
-            greenGradient.SetKeys(colorKeys, alphaKeys);
-            line.validColorGradient = greenGradient;
-            line.invalidColorGradient = greenGradient;
-        } else {
-            Gradient redGradient = new Gradient();
-            GradientColorKey[] colorKeys = new GradientColorKey[2];
-            colorKeys[0].color = Color.red;
-            colorKeys[0].time = 0f;
-            colorKeys[1].color = Color.red;
-            colorKeys[1].time = 1f;
-            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
-            alphaKeys[0].alpha = 1f;
-            alphaKeys[0].time = 0f;
-            alphaKeys[1].alpha = 1f;
-            alphaKeys[1].time = 1f;
-            redGradient.SetKeys(colorKeys, alphaKeys);
-            line.validColorGradient = redGradient;
-            line.invalidColorGradient = redGradient;
-        }
+    private void Update()
+    {
+        triggerVal = trigger.action.ReadValue<float>();
+        if (triggerVal > 0.1f) {
+            var ray = new Ray(controller.transform.position, controller.transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+                Debug.Log("Hit point: " + hit.transform);
+            }
+        } 
     }
 }
