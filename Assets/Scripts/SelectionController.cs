7a89bedfddcba451029ef1ef0820f1f36890f93a
IndiSnacks <sahilsankur@gmail.com>
2024-04-24 20:42:32 -0700

    tool: added the XR sim, Feature: implmeted selection

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -14,24 +14,48 @@ public class SelectionController : MonoBehaviour
     [SerializeField]
     private InputActionReference trigger;
 
+   [SerializeField]
+    private InputActionReference grip;
+
     private float triggerVal;
+    private float gripVal;
     private Vector3 hitPosition;
     private LayerMask layerMask;
 
-    private void Update()
+
+
+    private void FixedUpdate()
     {
         triggerVal = trigger.action.ReadValue<float>();
+        gripVal = grip.action.ReadValue<float>();
+
         var ray = new Ray(controller.transform.position, controller.transform.forward);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
-            hitPosition = hit.point;
+        if(Physics.Raycast(ray, out hit, 100)){
             lineRenderer.SetPosition(0, controller.transform.position);
-            lineRenderer.SetPosition(1, hitPosition);
-        }
+            lineRenderer.SetPosition(1, hit.point);
 
-        if (triggerVal > 0.1f) {
-            lineRenderer.startColor = Color.red;
-            lineRenderer.endColor = Color.red;
-        } 
+            if(hit.collider.gameObject.tag.CompareTo("Interactable") == 0){
+                lineRenderer.startColor = Color.green;
+                lineRenderer.endColor = Color.green;
+
+                if(triggerVal > 0.1f){
+                    lineRenderer.startColor = Color.blue;
+                    hit.rigidbody.useGravity = false;
+                    hit.collider.gameObject.transform.position = controller.transform.position + controller.transform.forward * 0.1f;
+                }
+                else{
+                    hit.rigidbody.useGravity = true;
+                }
+
+                if(gripVal > 0.1f){
+                    lineRenderer.startColor = Color.yellow;
+                }
+            }
+            else{
+                lineRenderer.startColor = Color.white;
+                lineRenderer.endColor = Color.white;
+            }
+        }
     }
 }
